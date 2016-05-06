a80164ad54e4e654708ba1ed2fdb49b65fa17d46
Thomas <dorian@tmsp.ga>
2016-05-06 22:35:43 +0200

    Dropped useless field that does nothing

--- SpaceDock/formatting.py
+++ SpaceDock/formatting.py
@@ -7,7 +7,6 @@ def game_info(game):
         "publisher_id": game.publisher_id,
         "short_description": game.short_description,
         "description": game.description,
-        "created": game.created.isoformat(),
         "background": game.background,
         "bg_offset_x": game.bgOffsetX,
         "bg_offset_y": game.bgOffsetY,
