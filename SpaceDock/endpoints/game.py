8fa3fe31c145fe0b544156a113476dd9f1bbc2e8
Thomas <dorian@tmsp.ga>
2016-05-06 22:21:09 +0200

    list_games

--- SpaceDock/endpoints/game.py
+++ SpaceDock/endpoints/game.py
@@ -1,7 +1,19 @@
 from flask import jsonify, request
 from SpaceDock.objects import *
+from SpaceDock.formatting import game_info
 
 class GameEndpoints:
     def __init__(self, cfg, db):
         self.cfg = cfg
         self.db = db
+        
+    def list_games(self):
+        """
+        Displays a list of all games in the database.
+        """
+        results = list()
+        for game in Game.query.order_by(desc(Game.name)).all():
+            results.append(game_info(game))
+        return jsonify(results)
+        
+    list_games.api_path = "/api/games"
