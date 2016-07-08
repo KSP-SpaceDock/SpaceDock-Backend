72a05444dca99e47d46e60ac8686c3252aaea314
Dorian Stoll <dorian@tmsp.ga>
2016-07-08 17:13:14 +0200

    Trying to unify the API layout, and removing legacy json decorators

--- SpaceDock/endpoints/game.py
+++ SpaceDock/endpoints/game.py
@@ -1,26 +1,39 @@
-from flask import jsonify, request
-from flask_login import login_required, current_user
-from sqlalchemy import desc, asc
+from sqlalchemy import desc
+from SpaceDock.common import game_id
 from SpaceDock.objects import *
 from SpaceDock.formatting import game_info, game_version_info
-from SpaceDock.common import with_session, edit_object
-from flask_json import FlaskJSON, JsonError, json_response, as_json, as_json_p
-from flask.ext.cache import Cache
 
 class GameEndpoints:
     def __init__(self, cfg, db):
         self.cfg = cfg
         self.db = db.get_database()
 
-
-    @as_json
     def list_games(self):
         """
         Displays a list of all games in the database.
         """
         results = list()
         for game in Game.query.order_by(desc(Game.name)).filter(Game.active):
-            results.append({
+            results.append(game.short)
+        return {'error': False, 'count': len(results), 'data': results}
+
+    list_games.api_path = '/api/games'
+
+
+    def game_info(self, gameshort):
+        """
+        Displays information about a game. Required parameters: gameshort
+        """
+        # Get the games with the according gameshort
+        filter = Game.query.filter(Game.short == gameshort)
+
+        # Game doesn't exist
+        if len(filter.all()) == 0:
+            return {'error': True, 'reasons': ['The gameshort is invalid.']}, 400
+
+        # Game does exist
+        game = filter.first()
+        data =  {
             "id": game.id,
             "name": game.name,
             "publisher_id": game.publisher_id,
@@ -30,60 +43,43 @@ class GameEndpoints:
             "background": game.background,
             "short": game.short,
             "link": game.link
-            })
-        return {"count":len(results),"data":results}
+        }
+        return {'error': False, 'count': 1, 'data': data}
 
-    list_games.api_path = '/api/games'
+    game_info.api_path = '/api/games/<gameshort>'
 
 
-    @as_json
-    def game_info(self, gameshort):
+    def game_versions(self, gameshort):
         """
-        Displays information about a game. Required parameters: gameid
+        Displays information about the versions of a game. Required parameters: gameshort
         """
-        game = Game.query.filter(Game.short == gameshort).first()
-        return {"count":1,"data":{
-        "id": game.id,
-        "name": game.name,
-        "publisher_id": game.publisher_id,
-        "short_description": game.short_description,
-        "description": game.description,
-        "created": game.created,
-        "background": game.background,
-        "short": game.short,
-        "link": game.link
-        }}
+        if not Game.query.filter(Game.short == gameshort).first():
+            return {'error': True, 'reasons': ['The gameshort is invalid.']}, 400
 
-    game_info.api_path = '/api/game/<gameshort>'
+        # Get the ID
+        gameid = game_id(gameshort)
 
-
-    @as_json
-    def game_versions(self, gameid):
-        """
-        Displays information about the versions of a game. Required parameters: gameid
-        """
-        if not gameid.isdigit() or not Game.query.filter(Game.id == int(gameid)).first():
-            return {'error': True, 'idErrors': 'The number you entered is not a valid ID.'}, 400
-   
-        # get game versions        
-        versions = GameVersion.query.filter(GameVersion.game_id == int(gameid)).all()
+        # get game versions
+        versions = GameVersion.query.filter(GameVersion.game_id == gameid).all()
 
         # Format them
-        result = list()
+        results = list()
         for version in versions:
-            result.append(game_version_info(version))
-        return {'error': False, 'game_versions': result}
+            results.append(game_version_info(version))
+        return {'error': False, 'count': len(results), 'data': results}
 
-    game_versions.api_path = '/api/game/<gameid>/versions'
+    game_versions.api_path = '/api/games/<gameshort>/versions'
 
 
-    @as_json
-    def game_mods(self, gameid):
+    def game_mods(self, gameshort):
         """
-        Displays a list of all mods added for this game. Required parameters: gameid
+        Displays a list of all mods added for this game. Required parameters: gameshort
         """
-        if not gameid.isdigit() or not Game.query.filter(Game.id == int(gameid)).first():
-            return {'error': True, 'idErrors': 'The number you entered is not a valid ID.'}, 400
+        if not Game.query.filter(Game.short == gameshort).first():
+            return {'error': True, 'reasons': ['The gameshort is invalid.']}, 400
+
+        # Get the ID
+        gameid = game_id(gameshort)
 
         # Get mods
         mods = Mod.query.filter(Mod.game_id == int(gameid)).all()
@@ -92,18 +88,20 @@ class GameEndpoints:
         result = dict()
         for mod in mods:
             result[str(mod.id)] = mod.name
-        return {'error': False, 'mods': result}
+        return {'error': False, 'count': len(result), 'data': result}
 
-    game_mods.api_path = '/api/game/<gameid>/mods'
+    game_mods.api_path = '/api/games/<gameshort>/mods'
 
 
-    @as_json
-    def game_modlists(self, gameid):
+    def game_modlists(self, gameshort):
         """
-        Displays all mod lists this game knows. Required parameters: gameid
+        Displays all mod lists this game knows. Required parameters: gameshort
         """
-        if not gameid.isdigit() or len(Game.query.filter(Game.id == int(gameid)).all()) == 0:
-            return {'error': True, 'idErrors': 'The number you entered is not a valid ID.'}, 400
+        if not Game.query.filter(Game.short == gameshort).first():
+            return {'error': True, 'reasons': ['The gameshort is invalid.']}, 400
+
+        # Get the ID
+        gameid = game_id(gameshort)
 
         # Get mod lists
         game = Game.query.filter(Game.id == int(gameid)).first()
@@ -113,12 +111,13 @@ class GameEndpoints:
         result = dict()
         for ml in modlists:
             result[str(ml.id)] = ml.name
-        return {'error': False, 'modlists': result}
+        return {'error': False, 'count': len(result), 'data': result}
 
-    game_modlists.api_path = '/api/game/<gameid>/modlists'
+    game_modlists.api_path = '/api/games/<gameshort>/modlists'
 
 
     #TODO: Move this to admin maybe?
+    #TODO: Redo with propper access control
     #@accessrequired
     #@with_session
     #def edit_game(self, gameid):
