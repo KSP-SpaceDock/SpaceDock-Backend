222f5570a92fad80019492be6df5fd023bf448b1
Dorian Stoll <dorian@tmsp.ga>
2016-07-16 21:18:37 +0200

    Move configs to subdirectory
    Makes everything cleaner

--- SpaceDock/config.py
+++ SpaceDock/config.py
@@ -3,27 +3,27 @@ from configparser import ConfigParser
 class Config:
     def __init__(self):
         self._config = ConfigParser()
-        self._config.readfp(open('config.ini'))
+        self._config.readfp(open('config/config.ini'))
         self._env = self._config['meta']['environment']
-        
+
     def get_environment(self):
         """
         Returns the current environment
         """
         return self._env
-    
+
     def get(self, key):
         """
         Returns a string
         """
         return self._config[self._env][key]
-    
+
     def geti(self, key):
         """
         Returns an integer
         """
         return int(self.get(key))
-    
+
     def getb(self, key):
         """
         Returns a boolean
@@ -31,16 +31,15 @@ class Config:
         if self.get(key).lower() in ['true', 't', 'yes', 'y', 1]:
             return True
         return False
-    
+
     def getf(self, key):
         """
         Returns a float
         """
         return float(self.get(key))
-    
+
     def __getitem__(self, key):
         """
         Returns a string, indexer access
         """
         return self.get(key)
-    
