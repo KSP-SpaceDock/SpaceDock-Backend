aa1a15f0f1b307ddaae42fd542c12bcbaf284225
Dorian Stoll <dorian@tmsp.ga>
2016-08-01 19:40:41 +0200

    Avoid weird dependencies

--- SpaceDock/database.py
+++ SpaceDock/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, String
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from SpaceDock.app import cfg
+from SpaceDock.config import cfg
 
 import json
 
