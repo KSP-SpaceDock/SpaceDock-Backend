cf59645b708277f5f0c5dcc8d110df616afc65c4
Dorian Stoll <dorian@tmsp.ga>
2016-08-04 12:43:21 +0200

    Add example mods

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 # Sets up a barebone instance of SpaceDock Backend
 
+from werkzeug.utils import secure_filename
 from SpaceDock.config import cfg
 from SpaceDock.database import db
 from SpaceDock.common import *
 from SpaceDock.objects import *
+from zipfile import ZipFile
+
+import os.path
 
 # Makes a new User
 def new_user(name, password, email, admin):
@@ -13,7 +17,7 @@ def new_user(name, password, email, admin):
     # Setup roles
     user.add_roles(name)
     db.commit()
-    role = Role.query.filter(Role.name == name.lower()).first()
+    role = Role.query.filter(Role.name == name).first()
     role.add_abilities('user-edit', 'mods-add', 'logged-in')
     role.add_param('user-edit', 'userid', user.id)
     role.add_param('mods-add', 'gameshort', '.*')
@@ -99,25 +103,86 @@ def new_game_admin(name, password, email, game):
     db.commit()
     return user
 
+# Creates a new mod
+def new_mod(name, user, game, license):
+    user = User.query.filter(User.username == user).first()
+    game = Game.query.filter(Game.short == game).first()
+
+    # Create new object
+    mod = Mod(name, user.id, game.id, license)
+
+    # Roles
+    user.add_roles(name)
+    role = Role.query.filter(Role.name == name).first()
+    role.add_abilities('mods-edit', 'mods-remove')
+    role.add_param('mods-edit', 'modid', str(mod.id))
+    role.add_param('mods-remove', 'name', name)
+
+    # Assign
+    db.add(role)
+    db.add(mod)
+
+    # Commit and return
+    db.commit()
+    return mod
+
+# Creates a new version for the mod
+def new_mod_version(modname, friendly_version, game, gameversion, beta):
+    mod = Mod.query.filter(Mod.name == modname).first()
+    game = GameVersion.query.filter(Game.query.filter(Game.short == game).first().id == GameVersion.game_id).filter(GameVersion.friendly_version == gameversion).first()
+
+    # Path
+    filename = secure_filename(mod.name) + '-' + secure_filename(friendly_version) + '.zip'
+    base_path = os.path.join(secure_filename(mod.user.username) + '_' + str(mod.user_id), secure_filename(mod.name))
+    full_path = os.path.join(cfg['storage'], base_path)
+    if not os.path.exists(full_path):
+        os.makedirs(full_path)
+    path_ = os.path.join(full_path, filename)
+
+    # Create the object
+    version = ModVersion(mod.id, friendly_version, game.id, path_, beta)
+
+    # Save data
+    zip = ZipFile(path_, 'w')
+    zip.writestr('SUPRISE.txt', ('As it seems, you downloaded ' + modname + ' ' + friendly_version).encode('utf-8'))
+    zip.close()
+
+    # add and commit
+    db.add(version)
+    db.commit()
+    return version
+    
 
 
-# Setup the DB
-admin = new_user('Administrator', 'admin', 'admin@noname.net', True)
-user = new_user('SpaceDockUser', 'user', 'user@noname.net', False)
+# Only run this when the file is run directly
+if __name__ == '__main__':
 
-# Game 1
-game_KSP = new_game('Kerbal Space Program', 'kerbal-space-program', 'Squad MX')
-version_112 = new_version('kerbal-space-program', '1.1.2', False)
-version_113 = new_version('kerbal-space-program', '1.1.3', False)
-version_12 = new_version('kerbal-space-program', '1.2', True)
+    # Setup the DB
+    admin = new_user('Administrator', 'admin', 'admin@noname.net', True)
+    user = new_user('SpaceDockUser', 'user', 'user@noname.net', False)
 
-# Game 2
-game_Factorio = new_game('factorio', 'factorio', 'Wube Software')
-version_012 = new_version('factorio', '0.12', False)
+    # Game 1
+    game_KSP = new_game('Kerbal Space Program', 'kerbal-space-program', 'Squad MX')
+    version_112 = new_version('kerbal-space-program', '1.1.2', False)
+    version_113 = new_version('kerbal-space-program', '1.1.3', False)
+    version_12 = new_version('kerbal-space-program', '1.2', True)
 
-# Game admins
-ksp_admin = new_game_admin('GameAdminKSP', 'gameadminksp', 'gameadminksp@noname.net', 'kerbal-space-program')
-fac_admin = new_game_admin('GameAdminFAC', 'gameadminfac', 'gameadminfac@noname.net', 'factorio')
+    # Game 2
+    game_Factorio = new_game('Factorio', 'factorio', 'Wube Software')
+    version_012 = new_version('factorio', '0.12', False)
+
+    # Game admins
+    ksp_admin = new_game_admin('GameAdminKSP', 'gameadminksp', 'gameadminksp@noname.net', 'kerbal-space-program')
+    fac_admin = new_game_admin('GameAdminFAC', 'gameadminfac', 'gameadminfac@noname.net', 'factorio')
+
+    # Mods
+    mod_ksp_1 = new_mod('DarkMultiPlayer', 'SpaceDockUser', 'kerbal-space-program', 'MIT')
+    mod_ksp_2 = new_mod('CookieEngine', 'GameAdminKSP', 'kerbal-space-program', 'GPL')
+
+    # Versions
+    mod_ksp_1_1 = new_mod_version('DarkMultiPlayer', '0.1', 'kerbal-space-program', '1.1.2', False)
+    mod_ksp_1_2 = new_mod_version('DarkMultiPlayer', '0.2', 'kerbal-space-program', '1.1.3', True)
+    mod_ksp_2_1 = new_mod_version('CookieEngine', '1.2', 'kerbal-space-program', '1.1.3', False)
 
 
     
