dab70bff4519001b8f1ec81afa8808ce14c5c125
Dorian Stoll <dorian@tmsp.ga>
2016-08-26 14:04:40 +0200

    Remove the debug-prefix, it is used always

--- SpaceDock.py
+++ SpaceDock.py
@@ -3,4 +3,4 @@ from SpaceDock.config import cfg
 
 # Start up the app
 if __name__ == '__main__':
-    app.run(host = cfg['debug-host'], port = cfg.geti('debug-port'))
+    app.run(host = cfg['host'], port = cfg.geti('port'))
