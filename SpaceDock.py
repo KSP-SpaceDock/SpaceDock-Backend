5c3887f57b4b8fd4ea02dbde3a70f82261ff4172
Dorian Stoll <dorian@tmsp.ga>
2016-09-23 19:56:27 +0200

    Add option for threading in standalone mod (i.e. no app server like gunicorn)

--- SpaceDock.py
+++ SpaceDock.py
@@ -3,4 +3,4 @@ from SpaceDock.config import cfg
 
 # Start up the app
 if __name__ == '__main__':
-    app.run(host = cfg['host'], port = cfg.geti('port'))
+    app.run(host = cfg['host'], port = cfg.geti('port'), threaded = cfg.getb('threaded'))
