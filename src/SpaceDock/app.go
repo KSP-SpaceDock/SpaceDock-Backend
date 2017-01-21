74cc259c83b132bed89de6ad41871b6570510463
Dorian Stoll <dorian@tmsp.ga>
2017-01-21 17:04:34 +0100

    Create the webserver for the routes

--- src/SpaceDock/app.go
+++ src/SpaceDock/app.go
@@ -4,24 +4,41 @@
 
  SpaceDock Backend is licensed under the Terms of the MIT License.
  Copyright (c) 2017 Dorian Stoll (ThomasKerman/TMSP), RockyTV
-*/
+ */
 
 package SpaceDock
 
-import "log"
+import (
+    "github.com/kataras/iris"
+    "log"
+    "strconv"
+)
+
+/*
+ The webserver that will listen for Requests
+ */
+var app iris.Framework
 
 /*
  Entrypoint wrapper that is called from the main() function.
 
  This will load the config, establish a connection to the database
  and startup the webserver to serve JSON
-*/
+ */
 func Run() {
     log.Print("SpaceDock-Backend -- Version: {$VERSION}")
     log.Print("* Loading configuration")
     LoadSettings()
 
-    // Debug
-    log.Print(settings.SiteName)
+    // Connect to the database
+    log.Print("* Establishing Database connection")
 
+    // Create the App
+    log.Print("* Initializing Iris-Framework")
+    app = *iris.New(iris.Configuration{IsDevelopment: settings.Debug })
+
+    // Load routes here
+
+    // Start listening
+    app.Listen(settings.Host + ":" + strconv.Itoa(settings.Port))
 }
