785a29f1febd028f3065bc739d475b50e9a26ed2
Dorian Stoll <dorian@tmsp.ga>
2017-01-22 00:44:59 +0100

    Middleware for Authentification

---
+++ src/SpaceDock/middleware/sessionauth.go
@@ -0,0 +1,54 @@
+/*
+ SpaceDock Backend
+ API Backend for the SpaceDock infrastructure to host modfiles for various games
+
+ SpaceDock Backend is licensed under the Terms of the MIT License.
+ Copyright (c) 2017 Dorian Stoll (ThomasKerman/TMSP), RockyTV
+ */
+
+package middleware
+
+import (
+    "SpaceDock/objects"
+    "github.com/kataras/iris"
+)
+
+func LoginRequired(ctx *iris.Context) {
+    userID, err := ctx.Session().GetInt("SessionID")
+    if err == nil {
+        var user objects.User
+        err = user.GetById(userID)
+        if !(err == nil && user.IsAuthenticated()) {
+            ctx.SetStatusCode(iris.StatusUnauthorized)
+            return
+        }
+    } else {
+        ctx.SetStatusCode(iris.StatusUnauthorized)
+        return
+    }
+    ctx.Next()
+}
+
+func LoginUser(ctx *iris.Context, user objects.User) {
+    user.Login()
+    ctx.Session().Set("SessionID", user.ID)
+}
+
+func LogoutUser(ctx *iris.Context, user objects.User) {
+    user.Logout()
+    ctx.Session().Delete("SessionID")
+}
+
+func CurrentUser(ctx *iris.Context) objects.User {
+    userID, err := ctx.Session().GetInt("SessionID")
+    if err == nil {
+        var user objects.User
+        err = user.GetById(userID)
+        if err != nil {
+            return user
+        } else {
+            return nil
+        }
+    }
+    return nil
+}
--- src/SpaceDock/objects/user.go
+++ src/SpaceDock/objects/user.go
@@ -13,6 +13,7 @@ import (
     "github.com/jinzhu/gorm"
     "time"
     "SpaceDock"
+    "errors"
 )
 
 type User struct {
@@ -26,6 +27,7 @@ type User struct {
     Confirmation string `gorm:"size:128"`
     PasswordReset string `gorm:"size:128"`
     PasswordResetExpiry time.Time
+    Authed bool
 }
 
 func (user User) SetPassword(password string) {
@@ -33,4 +35,29 @@ func (user User) SetPassword(password string) {
     user.Password, _ = bcrypt.Hash(password, salt)
     SpaceDock.Database.Save(user)
 }
+
+func (user User) IsAuthenticated() bool {
+    return user.Authed
+}
+
+func (user User) Login() {
+    user.Authed = true
+    SpaceDock.Database.Save(user)
+}
+
+func (user User) Logout() {
+    user.Authed = false
+    SpaceDock.Database.Save(user)
+}
+
+func (user User) UniqueId() interface{} {
+    return user.ID
+}
+
+func (user User) GetById(id interface{}) error {
+    SpaceDock.Database.First(&user, id)
+    if user.Username != "" {
+        return errors.New("Invalid user ID")
+    }
+    return nil
 }
