14bfd424da2c32c8ce9d9c3afca51c9f46fc4917
Dorian Stoll <dorian@tmsp.io>
2017-02-03 16:13:03 +0100

    Add the parameter API

--- src/SpaceDock/objects/role.go
+++ src/SpaceDock/objects/role.go
@@ -12,6 +12,8 @@ import (
     "SpaceDock"
     "errors"
     "github.com/jinzhu/gorm"
+    "encoding/json"
+    "SpaceDock/utils"
 )
 
 type Role struct {
@@ -67,4 +69,62 @@ func (role Role) GetAbilities() []Ability {
         value[index] = ability
     }
     return value
+}
+
+func (role Role) GetParams(ability string, param string) []string {
+    var temp map[string]map[string][]string
+    err := json.Unmarshal([]byte(role.Params), &temp)
+    if err != nil {
+        return nil
+    }
+    if _,ok := temp[ability]; ok {
+        if _,ok := temp[ability][param]; ok {
+            return temp[ability][param]
+        }
+    }
+    return nil
+}
+
+func (role Role) AddParam(ability string, param string, value string) error  {
+    var temp map[string]map[string][]string
+    err := json.Unmarshal([]byte(role.Params), &temp)
+    if err != nil {
+        return err
+    }
+    if _,ok := temp[ability]; !ok {
+        temp[ability] = map[string][]string{}
+    }
+    if _,ok := temp[ability][param]; !ok {
+        temp[ability][param] = []string{}
+    }
+    if ok,_ := utils.ArrayContains(value, temp[ability][param]); !ok {
+        temp[ability][param] = append(temp[ability][param], value)
+    }
+    val,err := json.Marshal(temp)
+    if err != nil {
+        return err
+    }
+    role.Params = string(val)
+    return nil
+}
+
+func (role Role) RemoveParam(ability string, param string, value string) error {
+    var temp map[string]map[string][]string
+    err := json.Unmarshal([]byte(role.Params), &temp)
+    if err != nil {
+        return err
+    }
+    if _,ok := temp[ability]; ok {
+        if _,ok := temp[ability][param]; ok {
+            if ok,index := utils.ArrayContains(value, temp[ability][param]); ok {
+                temp[ability][param] = append(temp[ability][param][:index], temp[ability][param][index+1:]...)
+            }
+        }
+    }
+    val,err := json.Marshal(temp)
+    if err != nil {
+        return err
+    }
+    role.Params = string(val)
+    return nil
 }
