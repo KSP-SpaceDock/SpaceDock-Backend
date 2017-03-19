e3935c82bc3c8ee5c2fd671a975c3e6537ce0f10
Dorian Stoll <dorian@tmsp.io>
2017-03-19 15:29:10 +0100

    Add the modlist objects

---
+++ src/SpaceDock/objects/modlist.go
@@ -0,0 +1,52 @@
+/*
+ SpaceDock Backend
+ API Backend for the SpaceDock infrastructure to host modfiles for various games
+
+ SpaceDock Backend is licensed under the Terms of the MIT License.
+ Copyright (c) 2017 Dorian Stoll (StollD), RockyTV
+ */
+
+package objects
+
+import "SpaceDock"
+
+type ModList struct {
+    Model
+
+    User             User `json:"-" spacedock:"lock"`
+    UserID           uint `json:"user" spacedock:"lock"`
+    Game             Game `json:"-" spacedock:"lock"`
+    GameID           uint `json:"game" spacedock:"lock"`
+    Description      string `gorm:"size:100000"`
+    ShortDescription string `gorm:"size:1000"`
+    Name             string `json:"name" gorm:"size:1024;unique_index;not null"`
+    Mods             []ModListItem `json:"-" spacedock:"lock"`
+}
+
+func (s *ModList) AfterFind() {
+    if SpaceDock.DBRecursion == 2 {
+        return
+    }
+    isRoot := SpaceDock.DBRecursion == 0
+    SpaceDock.DBRecursion += 1
+    SpaceDock.Database.Model(s).Related(&(s.User), "User")
+    SpaceDock.Database.Model(s).Related(&(s.Game), "Game")
+    SpaceDock.Database.Model(s).Related(&(s.Mods), "Mods")
+    if isRoot {
+        SpaceDock.DBRecursion = 0
+    }
+}
+
+func NewModList(name string, user User, game Game) *ModList {
+    modlist := &ModList{
+        User: user,
+        UserID: user.ID,
+        Game: game,
+        GameID: game.ID,
+        Name: name,
+        Description: "",
+        ShortDescription: "",
+    }
+    modlist.Meta = "{}"
+    return modlist
+}
---
+++ src/SpaceDock/objects/modlistitem.go
@@ -0,0 +1,48 @@
+/*
+ SpaceDock Backend
+ API Backend for the SpaceDock infrastructure to host modfiles for various games
+
+ SpaceDock Backend is licensed under the Terms of the MIT License.
+ Copyright (c) 2017 Dorian Stoll (StollD), RockyTV
+ */
+
+package objects
+
+import (
+    "SpaceDock"
+)
+
+type ModListItem struct {
+    Model
+
+    Mod       Mod `json:"-" spacedock:"lock"`
+    ModID     uint `json:"mod" spacedock:"lock"`
+    ModList   ModList `json:"-" spacedock:"lock"`
+    ModListID uint `json:"mod_list" spacedock:"lock"`
+    SortIndex uint `json:"sort_index" spacedock:"lock"`
+}
+
+func (s *ModListItem) AfterFind() {
+    if SpaceDock.DBRecursion == 2 {
+        return
+    }
+    isRoot := SpaceDock.DBRecursion == 0
+    SpaceDock.DBRecursion += 1
+    SpaceDock.Database.Model(s).Related(&(s.Mod), "Mod")
+    SpaceDock.Database.Model(s).Related(&(s.ModList), "ModList")
+    if isRoot {
+        SpaceDock.DBRecursion = 0
+    }
+}
+
+func NewModListItem(mod Mod, list ModList) *ModListItem {
+    modlistitem := &ModListItem{
+        Mod: mod,
+        ModID: mod.ID,
+        ModList: list,
+        ModListID: list.ID,
+        SortIndex:0,
+    }
+    modlistitem.Meta = "{}"
+    return modlistitem
+}
