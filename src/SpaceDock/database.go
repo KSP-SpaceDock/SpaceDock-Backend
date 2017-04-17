c43119933798eb80b6613e62a52bc26ee3f01524
Dorian Stoll <dorian@tmsp.io>
2017-04-17 17:57:36 +0200

    I remembered why SQLite wasnt there

--- src/SpaceDock/database.go
+++ src/SpaceDock/database.go
@@ -13,7 +13,6 @@ import (
     _ "github.com/jinzhu/gorm/dialects/mssql"
     _ "github.com/jinzhu/gorm/dialects/mysql"
     _ "github.com/jinzhu/gorm/dialects/postgres"
-    _ "github.com/jinzhu/gorm/dialects/sqlite"
     "log"
 )
 
--- tools/migrate.go
+++ tools/migrate.go
@@ -15,7 +15,6 @@ import (
     _ "github.com/jinzhu/gorm/dialects/mssql"
     _ "github.com/jinzhu/gorm/dialects/mysql"
     _ "github.com/jinzhu/gorm/dialects/postgres"
-    _ "github.com/jinzhu/gorm/dialects/sqlite"
     "time"
 )
 
