538fa7c773f50558c4408ecca95c1f48992b4a9d
Dorian Stoll <dorian@tmsp.io>
2017-07-30 20:41:44 +0200

    Lets see if this works

--- build/fetch_plugins.sh
+++ build/fetch_plugins.sh
@@ -3,23 +3,25 @@ SOURCE_FILE="sdb.go"
 echo "Reading plugins.txt file..."
 
 # Implement plugin stuff
+FILE=""
 if [ -e "$PWD/build_$filename" ] ; then
     rm $PWD/build_$SOURCE_FILE
 fi
-touch $PWD/build_$SOURCE_FILE
 while IFS= read -r line; do
     if [ "$line" = ")" ]
     then
         if [ -e "$PWD/build/plugins.txt" ]
         then
             while IFS= read -r line2; do 
-                printf "    _ \"$line2\"\n" >> $PWD/build_$SOURCE_FILE
+                FILE="$FILE    _ \"$line2\"\n"
                 $GOPATH/bin/glide get $line2 || true
             done < $PWD/build/plugins.txt
         fi
     fi        
-    printf "$line\n" >> $PWD/build_$SOURCE_FILE
+    FILE="$FILE$line\n"
 done < $PWD/$SOURCE_FILE 
+touch $PWD/build_$SOURCE_FILE
+printf "$FILE" >> $PWD/build_$SOURCE_FILE 
 
 # We are done
 echo "Plugins successfully applied. Please run go build on 'build_$SOURCE_FILE' to complete the build process."
